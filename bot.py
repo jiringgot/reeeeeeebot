24dd5341f9b193e02e0a1227a45ea30eba22d515
Nissekissen <45173078+Nissekissen@users.noreply.github.com>
2021-05-06 12:42:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,26 +35,22 @@ def gimme_meme():
     return quote
 
 
-@bot.event
+@client.event
 async def on_ready():
     await client.change_presence(
         activity=discord.Activity(type=discord.ActivityType.watching, name="TiredCheeseBoi on Twitch | r/help"))
     print('We have logged in as {0.user}'.format(client))
 
 
-@bot.command(name='hello', help='Say hello to the bot')
-async def hello(ctx):
-    await ctx.send('Hello!')
-    return
-@bot.command(name='inspire-me', help='Send an inspirable quote')
-async def inspire_me(ctx):
-    quote = get_quote()
-    await ctx.send(quote)
-    return
-@bot.command(name='meme', help='Make the bot send a meme')
-async def meme(ctx):
-    quote = gimme_meme()
-    await ctx.send(quote)
+@client.event
+async def on_message(message):
+    if message.startswith(sign + 'hello'):
+        await message.channel.send('Hello!')
+    if message.startswith(sign + 'inspire-me'):
+        quote = get_quote()
+        await message.channel.send(quote)
+    if message.startswith(sign + 'help'):
+        embed=discord.Embed(title="REEEEEEEbot help", description="REEEEEEEbot is just an experiment made by REEEEEEEboi. Here are some commands: \n \n **r/hello** - Say hello to the bot \n \n **r/inspire-me** - make the bot send an inspirable quote \n \n **r/help** - You probably know what this one does :P \n \n **r/meme** - Give yourself a fresh meme from reddit. \n \n Well that's all for now, \n REEEEEEE", color=0xFF5733)
 
 
 client.run(bot_token)
