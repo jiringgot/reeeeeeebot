9f0b81ea23598bc5e5d5435617a78b86d904d6dd
Nissekissen <45173078+Nissekissen@users.noreply.github.com>
2021-05-06 20:28:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,40 +36,19 @@ def gimme_meme():
     return quote
 
 
-@client.event
-async def on_ready():
-    await client.change_presence(
-        activity=discord.Activity(type=discord.ActivityType.watching, name="TiredCheeseBoi on Twitch | r/help"))
-    print('We have logged in as {0.user}'.format(client))
+class Bot(commands.AutoShardedBot):
+
+    def __init__(self):
+        super().__init__(command_prefix="~")
+
+    @commands.command()
+    async def test(self, ctx):
+        await ctx.send("some random text")
+
+    def run(self):
+        super().run(bot_token)
 
 
-#@client.event
-#async def on_message(message):
-#    if message.content.startswith(sign + 'hello'):
-#        await message.channel.send('Hello!')
-#    if message.content.startswith(sign + 'inspire-me'):
-#        quote = get_quote()
-#        await message.channel.send(quote)
-#    if message.content.startswith(sign + 'meme'):
-#        meme = gimme_meme()
-#        await message.channel.send(meme)
-#    if message.content.startswith(sign + 'help'):
- #       embed=discord.Embed(title="REEEEEEEbot help", description="REEEEEEEbot is just an experiment made by REEEEEEEboi. Here are some commands: \n \n **r/hello** - Say hello to the bot \n \n **r/inspire-me** - make the bot send an inspirable quote \n \n **r/help** - You probably know what this one does :P \n \n **r/meme** - Give yourself a fresh meme from reddit. \n \n Well that's all for now, \n REEEEEEE", color=0xFF5733)
-
-@commands.command()
-async def hello(ctx):
-    await ctx.send('Hello!')
-@commands.command()
-async def inspire_me(ctx):
-    quote = get_quote()
-    await ctx.send(quote)
-@commands.command()
-async def meme(ctx):
-    meme = gimme_meme()
-    await ctx.send(meme)
-
-bot.add_command(hello)
-bot.add_command(inspire_me)
-bot.add_command(meme)
-
-client.run(bot_token)
+if __name__ == "__main__":
+    bot = Bot()
+    bot.run()
