b16f511765e6e421fd80874dbeb6e26e183d1400
Nissekissen <45173078+Nissekissen@users.noreply.github.com>
2021-05-13 15:38:52 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,7 +78,7 @@ async def send_meme(ctx):
     embedVar = discord.Embed(title=meme['title'], description=" ", url=meme['postLink'], color=0x00ff00)
     embedVar.set_author(name=meme['author'])
     embedVar.set_image(url=meme['url'])
-    embedVar.set_footer(text=str(reddit_arrows) + str(meme['ups']))
+    embedVar.set_footer(text=str(meme['ups']))
     await ctx.send(embed=embedVar)
 
 @bot.command(name="ban", brief="Ban a member.", description="Ban a member. Can only be used by an adminstrator.")
