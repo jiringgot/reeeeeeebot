76cbc670630591933b0cab244304a285c7d49364
Nissekissen <45173078+Nissekissen@users.noreply.github.com>
2021-05-16 00:16:40 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     print(message.channel)
-    if message.channel == 'counting':
+    if message.channel == '<#counting>':
         print('pog')
         if message.startswith(str(int(count)+1)):
             count += 1
