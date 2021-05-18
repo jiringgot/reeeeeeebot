da8b00a05787b1084ef93f4a9f43312fee1aec2a
Nissekissen <45173078+Nissekissen@users.noreply.github.com>
2021-05-18 10:54:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -120,7 +120,7 @@ async def on_message(message):
     global count
     if message.channel == bot.get_channel(843246928161669141):
         if message.content == str(count):
-            count += 1
+            int(count) += 1
             print('Counted')
             update_count()
         else:
