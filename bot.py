cb1f70342f45118dbee9ec5f8362a7136bbe365e
Nissekissen <45173078+Nissekissen@users.noreply.github.com>
2021-05-20 23:12:53 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -97,15 +97,16 @@ async def ban(ctx, member : discord.Member, *, reason = None):
     await member.ban(reason = reason)
     
 
-@tasks.loop(minute=1)
+@tasks.loop(minutes=1)
 async def check_status():
+    global statuses
     api_statuses = get_status()
     for i in range(len(api_statuses)):
         if statuses[i] != api_statuses[i]['status']:
             statuses[i] = api_statuses[i]['status']
             os.environ['status'][i] = statuses[i]
             channel = bot.get_channel(802858193319493652)
-            await channel.send(api_statuses[i]['name'] + " is now " + statuses[i]
+            await channel.send(api_statuses[i]['name'] + " is now " + statuses[i])
     
 #The below code unbans player.
 @bot.command(name="unban", brief="Unban a member.", description="Unban a member. Can only be used by an administrator.")
