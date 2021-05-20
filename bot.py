950ffa550a72b2616413643247e5e6e273d5b1a2
Nissekissen <45173078+Nissekissen@users.noreply.github.com>
2021-05-20 23:44:21 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -105,7 +105,7 @@ def check_status():
         print(api_statuses[i]['status'])
         if statuses[i] != api_statuses[i]['status']:
             statuses[i] = api_statuses[i]['status']
-            os.environ['status'][i] = json.dumps(statuses[i])
+            os.environ['status'][i] = json.loads(statuses[i])
             returnList.append(api_statuses[i])
     return returnList
     
